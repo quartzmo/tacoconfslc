b62b96222bb7e01845c8cbf03e7fe8eb2e7bd922
Chris Smith <quartzmo@gmail.com>
2013-06-14 15:28:46 -0600

    first pass complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,65 @@
-console.log('\'Allo \'Allo!');
+/*
+    Countdown initializer
+*/
+$(function() {
+    var now = new Date();
+    var countTo = new Date(2013, 6-1, 22);
+    $('.timer').countdown(countTo, function(event) {
+        var $this = $(this);
+        switch(event.type) {
+            case "seconds":
+            case "minutes":
+            case "hours":
+            case "days":
+            case "weeks":
+            case "daysLeft":
+                $this.find('span.'+event.type).html(event.value);
+                break;
+            case "finished":
+                $this.hide();
+                break;
+        }
+    });
+});
+
+/*
+    Progress bar
+*/
+var percentage = $('.progress .bar').attr("data-percentage");
+$('.progress .bar').animate({width: (percentage)+'%'}, 1000);
+
+
+/*
+    Subscription form
+*/
+jQuery(document).ready(function() {
+
+    $('.success-message').hide();
+    $('.error-message').hide();
+
+    $('.subscription-form-container form').submit(function() {
+        var postdata = $('.subscription-form-container form').serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'assets/sendmail.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                if(json.valid == 0) {
+                    $('.success-message').hide();
+                    $('.error-message').hide();
+                    $('.error-message').html(json.message);
+                    $('.error-message').fadeIn();
+                }
+                else {
+                    $('.error-message').hide();
+                    $('.success-message').hide();
+                    $('.subscription-form-container form').hide();
+                    $('.success-message').html(json.message);
+                    $('.success-message').fadeIn();
+                }
+            }
+        });
+        return false;
+    });
+});
